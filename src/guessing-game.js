cad5cb18d4d97758856a6ed026e769fa5c99a860
SPeslyak <peslyak.sergey@gmail.com>
2021-08-30 13:44:42 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,17 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        this.mm = Math.ceil((this.max + this.min) / 2);
+        return this.mm;
     }
-
     lower() {
-
+        this.max = this.mm;
     }
-
     greater() {
-
+        this.min = this.mm;
     }
 }
 
